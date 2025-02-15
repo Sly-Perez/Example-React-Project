c4def72cb6cdd8f79cdd33a5bdab0fde615685db
Sly-Perez <elaipereza@gmail.com>
2025-02-15 15:44:48 -0500

    data folder added: MainNavbarData.jsx (we will pull in data to the navbar options and links from a json file)

---
+++ src/data/MainNavbarData.jsx
@@ -0,0 +1,53 @@
+export const navbarList = [
+    {
+        option: "Investments",
+        url: "/investments",
+        title: "Read our investments"
+    },
+    {
+        option: "Suppliers",
+        url: "/suppliers",        
+        title: "Our suppliers"
+    },
+    {
+        option: "Employees",
+        url: "/employees",
+        title: "Our employees"
+    },
+    {
+        option: "Store",
+        url: "/store",
+        title: "Buy our products"
+    },
+    {
+        option: "Customers",
+        url: "/customers",
+        title: "Our Customers"
+    },
+    {
+        option: "Directors",
+        url: "/directors",
+        title: "Movie Directors"
+    }
+];
+
+export const navBarListRight = [
+    {
+        option: "Cart",
+        url: "/cart",
+        title: "Shopping Cart",
+        icon: "bi bi-basket-fill cart-icon"
+    },
+    {
+        option: "Selected",
+        url: "/selectedSuppliers",
+        title: "Selected Suppliers",
+        icon: "bi bi-hammer"
+    },
+    {
+        option: "Log In",
+        url: "/login",
+        title: "Access to Platform",
+        icon: "bi bi-person-circle"
+    }
+];
