97d21a744f8f6e0ee6e1eaf0dea66ac7256adc57
Sly-Perez <elaipereza@gmail.com>
2025-02-15 15:45:38 -0500

    home folder added: aboutUs.css, aboutUs.jsx, MainBanner.jsx, News.jsx added

---
+++ src/home/News.jsx
@@ -0,0 +1,29 @@
+
+function News() {
+  return (
+    <section id="news" className="padded">
+        <div className="container">
+          <h2>News</h2>
+          <div className="row">
+            <div className="col">
+              <h3>News 1</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, debitis recusandae? Quam, quaerat sint! Qui, eius. Deserunt placeat deleniti perferendis, iusto minus architecto quae pariatur modi assumenda velit error voluptates aut iure.</p>
+              <p>Soluta, omnis obcaecati doloribus in ut sit non nesciunt ratione! Ab deserunt aliquam esse totam repellendus? Quia, facilis earum repellendus reiciendis minus dignissimos voluptatem enim eaque asperiores est temporibus aliquid suscipit animi.</p>
+            </div>
+            <div className="col">
+              <h3>News 2</h3>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum, cum, recusandae inventore maiores libero debitis molestias ipsam dolor distinctio fugiat consequuntur vitae sequi porro minus voluptatum possimus expedita excepturi deserunt fuga. Labore.</p>
+              <p>Id obcaecati nobis nulla laboriosam minus nam ex voluptas suscipit facilis accusamus magnam ducimus repellat hic atque tempora reiciendis tempore sequi odio porro, repudiandae cupiditate neque voluptatum cumque officiis. Dolor, ipsum cumque!</p>
+            </div>
+            <div className="col">
+              <h3>News 3</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae quod laudantium soluta quaerat et exercitationem provident eligendi doloribus veniam, enim voluptatem error id, possimus tempore atque nesciunt maxime ab cumque? Perferendis, laboriosam.</p>
+              <p>Omnis, praesentium ea minus asperiores beatae illo voluptates non minima! Explicabo exercitationem corrupti cum at eaque quibusdam dolore alias corporis expedita, ducimus nisi hic vitae veritatis impedit, sed consectetur magnam commodi deleniti?</p>
+            </div>
+          </div>
+        </div>
+      </section>
+  )
+}
+
+export default News;
---
+++ src/home/aboutUs.jsx
@@ -0,0 +1,14 @@
+import './aboutUs.css';
+
+function AboutUs() {
+  return (
+    <section id="about-us" className="padded">
+        <div className="container">
+          <h2>About Us</h2>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus maiores ut ipsam vitae eligendi eaque iste expedita, sequi ex alias officia ducimus accusantium nulla repellat libero repellendus hic assumenda doloremque ipsum sed? Perspiciatis similique magnam vel dicta. Commodi distinctio nam odio, aspernatur blanditiis natus sapiente quas modi reiciendis fuga minima quia rem optio, ad cumque soluta beatae id necessitatibus architecto! Doloremque dicta accusamus laudantium explicabo atque, harum sit est unde consequuntur cupiditate consectetur quas veniam rem molestiae autem ratione impedit expedita fugiat! Distinctio et omnis voluptatum perferendis laborum officiis necessitatibus quia animi at repellat consectetur iste, voluptates ipsam labore odio sint? Id libero culpa assumenda deserunt excepturi? Adipisci fugiat, placeat commodi reiciendis est repellendus blanditiis, nihil dolorum, alias quae voluptate.</p>
+        </div>
+    </section>
+  )
+}
+
+export default AboutUs;
